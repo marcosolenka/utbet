275609e4f20c5517c629035c0bfc69b2bffe5eda
marcosolenka <marcosolenka@gmail.com>
2024-04-04 22:05:20 -0300

    start to connect API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+window.addEventListener("DOMContentLoaded", importHeader);
+catchCountryNames();
+getCountryFlag("Brazil");
+
 function importHeader() {
   fetch("/src/app/header/header.html")
     .then((response) => response.text())
@@ -7,5 +11,50 @@ function importHeader() {
     .catch((error) => console.error("Erro ao importar o cabeçalho:", error));
 }
 
-// utiliza a função importHeader no carregamento da pagina
-window.addEventListener("DOMContentLoaded", importHeader);
+async function catchCountryNames() {
+  try {
+    // Faz a requisição GET para a API Rest Countries usando fetch
+    const response = await fetch("https://restcountries.com/v3.1/all");
+
+    if (response.ok) {
+      const data = await response.json();
+
+      let countryNames = data.map((country) => country.name.common);
+      console.log(countryNames);
+    } else {
+      throw new Error("Erro ao buscar os dados da API");
+    }
+  } catch (error) {
+    console.error("Ocorreu um erro ao fazer a requisição:", error);
+  }
+}
+
+async function getCountryFlag(countryName) {
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+
+    if (response.ok) {
+      const data = await response.json();
+
+      const country = data.find(
+        (country) => country.name.common === countryName
+      );
+
+      // Verifica se o país foi encontrado
+      if (country) {
+        // Obtém a bandeira do país
+        const flag = country.flags.png;
+
+        console.log(`A bandeira de ${countryName} é: ${flag}`);
+      } else {
+        console.error(
+          `Não foi possível encontrar informações para o país "${countryName}".`
+        );
+      }
+    } else {
+      throw new Error("Erro ao buscar os dados da API");
+    }
+  } catch (error) {
+    console.error("Ocorreu um erro ao fazer a requisição:", error);
+  }
+}
